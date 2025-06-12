7726298f1534fec33314834a62396b638a4495e6
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-06-13 05:27:13 +0900

    バイナリをget-nhk-radio-urlに変更

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-// NHKラジオのリアルタイム・ストリーミングを保存する
+// NHKラジオのリアルタイム・ストリーミングURLを標準出力に出力する。
 use clap::{CommandFactory, Parser, Subcommand};
 use clap_complete;
-use nhk_radio_recorder::{RadioChannel, RadioLocation};
-use std::process::Command;
-use std::time::Duration;
-use wait_timeout::ChildExt; // This import is necessary for .wait_timeout()
+use get_nhk_radio_url::{RadioChannel, RadioLocation};
 
 /// コマンドライン引数の定義
 #[derive(Subcommand, Debug)]
@@ -18,14 +15,6 @@ pub enum Commands {
         /// チャンネル
         #[arg(value_enum, long, short)]
         channel: RadioChannel,
-
-        /// 番組名
-        #[arg(long, short)]
-        title: String,
-
-        /// 番組の長さ[分]
-        #[arg(long, short, default_value = "60")]
-        duration: u32,
     },
     /// コマンドライン補完スクリプトを生成
     Completion {
@@ -51,57 +40,13 @@ fn main() {
     // コマンドライン引数の解析
     match cli.command {
         // エアチェックを行う
-        Commands::Aircheck {
-            location,
-            channel,
-            title: program,
-            duration,
-        } => {
-            // 地域とチャンネルから適切なストリームURLを取得する。
-            let station_url = nhk_radio_recorder::get_station_url(location, channel);
-
-            // ffmpegコマンドにこのストリームURLを渡して、指定された番組名と長さでエアチェックを行う。
-            let mut child = Command::new("ffmpeg")
-                .arg("-y") // 既存のファイルを上書きする
-                .arg("-i")
-                .arg(station_url) // 入力ストリームURLを指定する
-                .arg("-c")
-                .arg("copy") // トランスエンコードせずにTSの中のデータをコピーする
-                .arg("-t") // 録音の長さ
-                .arg(format!("{}:00", duration)) // durationは分単位なので、秒に変換する必要がある
-                .arg(format!("{}.m4a", program)) // 出力ファイル名を指定する
-                .arg("-loglevel")
-                .arg("error") // エラーメッセージのみを表示する
-                .arg("-reconnect") // 再接続を有効にする
-                .arg("1")
-                .arg("-reconnect_at_eof") // EOFで再接続を有効にする
-                .arg("1")
-                .arg("-reconnect_on_network_error") // ネットワークエラーで再接続を有効にする
-                .arg("1")
-                .stderr(std::process::Stdio::from(
-                    // 標準エラー出力をファイルにリダイレクトする
-                    std::fs::File::create(format!("{}.stderr.log", program)).unwrap(),
-                ))
-                .stdout(std::process::Stdio::from(
-                    // 標準出力をファイルにリダイレクトする
-                    std::fs::File::create(format!("{}.stdout.log", program)).unwrap(),
-                ))
-                .spawn()
-                .expect("Command::new() should start successfully");
-            // サブプロセスの終了を待つ
-            let timeout = Duration::from_secs(60 * duration as u64 + 10); // 10秒余裕
-            match child.wait_timeout(timeout).expect("wait_timeout failed") {
-                Some(_status) => {
-                    // プロセス正常終了。
-                    // 特に何もしない。
-                }
-                None => {
-                    // タイムアウト
-                    child.kill().expect("failed to kill process");
-                    println!("Process killed due to timeout");
-                }
-            }
+        Commands::Aircheck { location, channel } =>
+        // 地域とチャンネルから適切なストリームURLを取得する。
+        {
+            // ストリームURLを出力する
+            println!("{}", get_nhk_radio_url::get_station_url(location, channel));
         }
+
         // コマンドライン補完スクリプトを生成する
         Commands::Completion { shell } => {
             // コマンドライン引数の体系をclapが解析するための構造体を生成する。
